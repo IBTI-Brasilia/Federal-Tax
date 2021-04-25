c05a125e206a80b2b2d7f7bea633913154194852
francocamila <imcamilafranco@gmail.com>
2021-04-25 19:25:29 -0300

    costumized form

--- pdf_kw_extractor/forms.py
+++ pdf_kw_extractor/forms.py
@@ -6,3 +6,9 @@ class UploadPdfForm(forms.ModelForm):
     class Meta:
         model = UploadPdf
         fields = ('document', )
+        widgets = {
+            'document': forms.FileInput(attrs={'style':'display: none;','class':'form-control', 'required': True})
+        }
+    #def __init__(self, *args, **kwargs):
+    #    super().__init__(*args, **kwargs)
+    #    self.fields['document'].widget.attrs.update({'position':'absolute', 'height':'1px', 'left':'0px', 'right':'0px', 'bottom':'0px', 'left':'0px', 'background': '#D3D8DD'})
