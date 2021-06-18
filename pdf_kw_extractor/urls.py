62ea7f168ead4d409496e1ab8448108fac4f36df
francocamila <imcamilafranco@gmail.com>
2021-06-18 13:52:48 -0300

    path fixed

--- pdf_kw_extractor/urls.py
+++ pdf_kw_extractor/urls.py
@@ -11,5 +11,5 @@ urlpatterns = [
     path("logout", views.logout_request, name="logout"),
     path('view_more/delete/<id>', views.delete_process),
     path('view_more/download_pdf/<id>', views.download_pdf, name='download_pdf'),
-    path('home/', HomeView.as_view()),
+    path('home/', HomeView.as_view(), name = "home"),
 ]
